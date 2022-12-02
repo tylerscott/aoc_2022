fd26a7f4a325aed353376e239a019fb9c450cf6c
Tyler McWatters <tyler@tylermcwatters.com>
2022-12-01 18:52:54 -0800

    Refactor Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,16 +1,22 @@
 
 fn main () {
     let input: &str = include_str!("../input/day1.txt");
+    
+    let max_calories = calorie_iterator(&input).max().unwrap();
+    let mut top_calories = calorie_iterator(&input).collect::<Vec<usize>>();
+    top_calories.sort_by(|a,b| b.cmp(a));
 
-    let line_parse: Option<usize> = input.split("\n\n").map(|line| line.split("\n")
-        .flat_map(|num: &str| num.parse::<usize>()).sum::<usize>()
-    ).max();
-
-    let mut second: Vec<usize> = input.split("\n\n").map(|line| line.split("\n")
-        .flat_map(|num: &str| num.parse::<usize>()).sum::<usize>()
-    ).collect::<Vec<usize>>();
-    second.sort_by(|a,b| b.cmp(a));
-
-    println!("first answer: {:?}", line_parse.unwrap());
-    println!("Second: {:?}", second.iter().take(3).sum::<usize>());
+    println!("Max Calories: {:?}", max_calories);
+    println!("Top Three: {:?}", top_calories.iter().take(3).sum::<usize>());
 }
+
+fn calorie_iterator(input: &str) -> impl Iterator<Item = usize> + '_ {
+    input.split("\n\n")
+        .map(|calories: &str| {
+        return calories
+                .split("\n")
+                .flat_map(str::parse::<usize>)
+                .sum::<usize>()
+        })
+}
+
